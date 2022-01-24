3527e632ac774c9ac2ab6bdff804155eb6b96b4e
ozreyyiz <hcefe.1@gmail.com>
2022-01-24 12:12:59 +0300

    level 1 updated.

--- lib/pages/quizpages/level2.dart
+++ lib/pages/quizpages/level2.dart
@@ -1,4 +1,11 @@
+import 'package:flag_quiz/constant.dart';
+import 'package:flag_quiz/db/flagsdao.dart';
+import 'package:flag_quiz/models/flags.dart';
+import 'package:flag_quiz/pages/mainpageup.dart';
+import 'package:flag_quiz/pages/resultpage.dart';
+import 'package:flag_quiz/widgets/quiz_answer_button.dart';
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
 
 class Level2 extends StatefulWidget {
   Level2({Key? key}) : super(key: key);
@@ -8,8 +15,170 @@ class Level2 extends StatefulWidget {
 }
 
 class _Level2State extends State<Level2> {
+  var questions = <Flag>[];
+  var wrongOptions = <Flag>[];
+  var allOptions = <Flag>[];
+  late Flag liveQuestions;
+
+  int correctCount = 0;
+  int wrongCount = 0;
+  int questionCount = 0;
+
+  String flagPhotoName =
+      "https://media.istockphoto.com/photos/close-up-of-usa-flag-picture-id511092533";
+  String buttonAtext = "";
+  String buttonBtext = "";
+  String buttonCtext = "";
+  String buttonDtext = "";
+
+  Future<void> loadQuestions() async {
+    liveQuestions = questions[questionCount];
+    flagPhotoName = liveQuestions.flag_photo;
+    wrongOptions = await Flagsdao().getFalseFlags(liveQuestions.flag_id);
+
+    allOptions.clear();
+    allOptions.add(liveQuestions);
+    allOptions.add(wrongOptions[0]);
+    allOptions.add(wrongOptions[1]);
+    allOptions.add(wrongOptions[2]);
+
+    buttonAtext = allOptions.elementAt(0).flag_name;
+    buttonBtext = allOptions.elementAt(1).flag_name;
+    buttonCtext = allOptions.elementAt(2).flag_name;
+    buttonDtext = allOptions.elementAt(3).flag_name;
+
+    setState(() {});
+  }
+
+  Future<void> getQuestions() async {
+    questions = await Flagsdao().getFlags();
+    loadQuestions();
+  }
+
+  void questionCountController() async {
+    questionCount = questionCount + 1;
+
+    if (questionCount != 10) {
+      loadQuestions();
+    } else {
+      Navigator.pushReplacement(
+          (this.context),
+          MaterialPageRoute(
+              builder: (context) => ResultPage(
+                    correctAnswer: correctCount,
+                  )));
+    }
+  }
+
+  void correctAnswerControl(String buttonText) {
+    if (liveQuestions.flag_name == buttonText) {
+      correctCount = correctCount + 1;
+    } else {
+      wrongCount = wrongCount + 1;
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getQuestions();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Container(
+              height: size.height,
+              width: size.width,
+              decoration: BoxDecoration(
+                color: Color(0xffEC8B6B),
+              ),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => MainPageUp()));
+                      },
+                      icon: Icon(
+                        Icons.close,
+                        color: mainTextColor,
+                        size: 40,
+                      ),
+                    ),
+                  ],
+                ),
+                Material(
+                  elevation: 20,
+                  child: Container(
+                    height: size.height / 5,
+                    width: size.width / 2,
+                    child: Image.asset("assets/$flagPhotoName"),
+                  ),
+                ),
+                Container(
+                  width: size.width / 1.5,
+                  child: Text(
+                    "Question $questionCount of 10",
+                    style: TextStyle(
+                      color: mainTextColor,
+                      fontSize: 20,
+                      fontFamily: "Lato",
+                    ),
+                  ),
+                ),
+                Column(
+                  children: [
+                    QuizAnswerButton(
+                      size: size,
+                      countryName: buttonAtext,
+                      fonk: () {
+                        correctAnswerControl(buttonAtext);
+                        questionCountController();
+                      },
+                    ),
+                    QuizAnswerButton(
+                      size: size,
+                      countryName: buttonBtext,
+                      fonk: () {
+                        correctAnswerControl(buttonBtext);
+                        questionCountController();
+                      },
+                    ),
+                    QuizAnswerButton(
+                      size: size,
+                      countryName: buttonCtext,
+                      fonk: () {
+                        correctAnswerControl(buttonCtext);
+                        questionCountController();
+                      },
+                    ),
+                    QuizAnswerButton(
+                      size: size,
+                      countryName: buttonDtext,
+                      fonk: () {
+                        correctAnswerControl(buttonDtext);
+                        questionCountController();
+                      },
+                    ),
+                  ],
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
